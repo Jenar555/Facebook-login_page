4fb31dec8f195aadc6b93971c686f66fd969c402
Rupakkumarjena <123276180+Jenar555@users.noreply.github.com>
2023-11-26 06:16:38 +0530

    Add files via upload

---
+++ wedev/facebk.js
@@ -0,0 +1,11 @@
+
+const loginButton = document.querySelector(".login");
+
+loginButton.addEventListener("click", function () {
+
+  const isSuccess = true;
+  if (isSuccess) {
+    alert("Login successful!");
+  }
+ }
+);
